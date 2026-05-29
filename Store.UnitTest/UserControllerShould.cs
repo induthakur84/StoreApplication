8c7cda728b277f724a692ce70c09f124fddca21b
Indu Bala <indubala@gmail.com>
2026-05-29 21:39:20 +0530

    Added new changes usercontroller

--- Store.UnitTest/UserControllerShould.cs
+++ Store.UnitTest/UserControllerShould.cs
@@ -1,89 +1,193 @@
 using NSubstitute;
 using Order.API.Controllers;
 using Order.Data.Interfaces;
-using Order.DTO.DTO.Request;
 using Order.DTO.DTO.Response;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Order.DTO.DTO.Request;
+using System.Security.Policy;
 
 namespace Store.UnitTest
 {
     public class UserControllerShould
     {
-
         private readonly IUserService _userMock;
-
         private readonly UserController _userController;
-        public UserControllerShould(IUserService userService)
+
+        public UserControllerShould()
         {
-            //Arrange
+            // Arrange
             _userMock = Substitute.For<IUserService>();
             _userController = new UserController(_userMock);
         }
 
         [Fact]
-        public async Task GivenRequest_WhenGetAll_ThenReturnReturnUsers()
+        public async Task GivenRequest_WhenGetAll_ThenReturnUsers()
         {
-            //AAA triple A pattern
-
-
-            //Arrange the data and the mock behavior
-
-            //Act
-
-            //Assert    
-
-
-
-            //Arrange the data and the mock behavior    
-
-
+            // Arrange
             var users = new List<UserResponse>
             {
-
-                new UserResponse { Id = 1, Name = "John Doe", Email = "ram@gmail.com" },
-                new UserResponse { Id = 2, Name = "Jane Smith", Email = "ram12@gmail.com" }
+                new UserResponse
+                {
+                    Id = 1,
+                    Name = "John Doe",
+                    Email = "ram@gmail.com"
+                },
+                new UserResponse
+                {
+                    Id = 2,
+                    Name = "Jane Smith",
+                    Email = "ram12@gmail.com"
+                }
             };
 
             _userMock.GetAll().Returns(users);
 
-
-
-
-            //Act
+            // Act
             var result = await _userController.GetAll();
 
-
-
-            //Assert
+            // Assert
             result.Should().NotBeNull();
 
-            //var okResult= Assert.IsType<OkObjectResult>(result);
+            var okResult = Assert.IsType<OkObjectResult>(result);
 
-            //var data= Assert.IsAssignableFrom<IEnumerable<UserResponse>>(okResult.Value);
+            var data = Assert.IsAssignableFrom<IEnumerable<UserResponse>>(okResult.Value);
 
-            //data.Should().HaveCount(2);
+            data.Should().HaveCount(2);
 
             await _userMock.Received(1).GetAll();
         }
 
-
         [Fact]
-        public async Task GivenRequest_WhenGetById_ThenNOTFoundindatabase()
+        public async Task GivenRequest_WhenGetById_ThenReturnNotFound()
         {
-
-            //arrange
+            // Arrange
             _userMock.GetById(1).Returns((UserResponse?)null);
 
-            //act
+            // Act
             var result = await _userController.GetById(1);
 
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
 
-            //assert
-
-            Assert.IsType<NotFoundResult>(result);
             await _userMock.Received(1).GetById(1);
+        }
+
+
+
+        [Fact]
+
+        public async Task GivenRequest_WhenGetById_ThenReturnUser()
+        {
+            // Arrange
+            var user = new UserResponse
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "ram@gmail.com"
+            };
+
+            _userMock.GetById(1).Returns(user);
+            //Act
+
+            var result = await _userController.GetById(1);
+            ///Assert
+
+            result.Should().NotBeNull();
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            okResult.Value.Should().BeEquivalentTo(user);
+            await _userMock.Received(1).GetById(1);
+        }
+
+        #region Create Tests
+
+        [Fact]
+        public async Task GivenRequest_WhenCreate_ThenReturnCreateUser()
+        {
+            // Arrange
+            var userRequest = new UserRequest
+            {
+                Name = "John Doe",
+                Email = "ram@gmail.com"
+            };
+
+            var userResponse = new UserResponse
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "ram@gmail.com"
+            };
+
+            _userMock.Create(userRequest).Returns(userResponse);
+
+
+
+            //Act   
+
+            var result = await _userController.Create(userRequest);
+
+
+
+            ///Assert
+            ///
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            okResult.Value.Should().BeEquivalentTo(userResponse);
 
         }
+
+        #endregion
+
+
+
+
+
+        [Fact]
+        public async Task GivenValidRequest_WhenUpdate_ThenReturnUpdatedUser()
+        {
+            // Arrange
+            var userRequest = new UserRequest
+            {
+                Name = "John Doe Updated",
+                Email = "update@gmail.com"
+            };
+
+            var response = new UserResponse
+            {
+                Id = 1,
+                Name = "John Doe Updated",
+                Email = "update@gmail.com",
+            };
+
+            _userMock.Update(1, userRequest).Returns(response);
+
+
+            //Act
+
+            var result = await _userController.Update(1, userRequest);
+
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            okResult.Value.Should().BeEquivalentTo(response);
+        }
+
+
+        //Delete Tests  
+
+        [Fact]
+        public async Task GivenValidRequest_WhenDelete_ThenReturnTrue()
+        {
+            // Arrange
+            _userMock.Delete(1).Returns(true);
+            // Act
+            var result = await _userController.Delete(1);
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            await _userMock.Received(1).Delete(1);
+        }
+       
+
     }
-    }
+}
